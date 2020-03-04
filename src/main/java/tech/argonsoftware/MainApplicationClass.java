0b22a51bd32445c3ff81ea3f5e698d6cc0cd2b83
Henry <regondolajosh@gmail.com>
2020-03-04 16:59:19 +0800

    main application

---
+++ src/main/java/tech/argonsoftware/MainApplicationClass.java
@@ -0,0 +1,13 @@
+package tech.argonsoftware;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplicationClass {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MainApplicationClass.class, args);
+
+    }
+}
